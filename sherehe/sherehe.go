77732a4250e6e35ff0cb6026f1a40a23bdb16e74
mrmuli <jayjaymuli00@gmail.com>
2021-02-13 20:32:55 +0100

    playing with arrays (which no one does...)

--- sherehe/sherehe.go
+++ sherehe/sherehe.go
@@ -1,1 +1,26 @@
 package main
+
+import "fmt"
+
+/*
+Zusha simply manipulates an array
+*/
+func Zusha() {
+	noma := [5]int{}
+	fmt.Println(noma)
+
+	// A basic way to add values in there
+	// manually this would be tedious
+	// as noma[0-4] = digit as noma[0] = 23
+	for i := 0; i <= 4; i++ {
+		noma[i] = i
+	}
+	// fmt.Println(noma)
+	// Range and Print content
+	for k, v := range noma {
+		fmt.Println(k, v)
+	}
+
+	// format printing: print type of the array
+	fmt.Printf("Type is %T\n", noma)
+}
